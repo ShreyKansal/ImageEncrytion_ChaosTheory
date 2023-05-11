623783ef2d6324f42b0d285edea9daf7328c66c0
Shrey Kansal <skansal801@gmail.com>
2023-05-11 18:39:11 +0530

    Delete rgb_split.py

--- rgb_split.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-image = Image.open('C:/Users/skans/OneDrive/Pictures/testimage.png')
-red, green, blue = image.split()
-red.save("red.jpg")
-green.save("green.jpg")
-blue.save("blue.jpg")
-
